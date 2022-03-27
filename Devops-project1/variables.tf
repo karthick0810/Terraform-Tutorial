2dc7c22d1886565a48b56b2419620b95b8fbc245
karthick0810 <91515284+karthick0810@users.noreply.github.com>
2022-03-27 23:14:28 +0530

    Update variables.tf

--- Devops-project1/variables.tf
+++ Devops-project1/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
   default = "us-east-2"
 }
 variable "ami_id" {
-  type = "map"
+  type = map
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
